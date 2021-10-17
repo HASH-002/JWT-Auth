7ea7106542d3b869991ddb21d17c85acfaec124b
HASH-002 <koserhasan002@gmail.com>
2021-10-17 22:20:52 +0530

    Error handling in routes

--- index.js
+++ index.js
@@ -1,44 +1,29 @@
 const express = require('express')
-const morgan = require('morgan')
-const createError =require('http-errors')
-
-require('dotenv').config()
-require('./helpers/init_mongodb')
-
-const authRoute = require('./routes/auth')
+const createError = require('http-errors')
+const { port } = require('./config')
 
 const app = express()
-app.use(morgan('dev')) // just after initialising app in development mode
-app.use(express.json()) // To parse incoming json data
-app.use(express.urlencoded({extended: true})) // To parse incoming form data  
 
-app.get('/',(req,res)=>{
-    res.send("Hello")
+const PORT = port || 5000
+
+app.get('/', async (req, res, next)=> {
+    res.send('Hello World')
 })
 
-app.use('/auth', authRoute)
-
-// If route not present
-app.use((req,res,next)=>{
-    // const error = new Error("Not Found");
-    // error.status = 404
-    // next(error)
-    next(createError.NotFound())
+app.use( async (req, res, next)=> {
+    next(createError.NotFound()) // passing error as first parameter
 })
 
-// Every error will be redirected to here from auth.js too using next() middleware
 app.use((err,req,res,next)=>{
-    res.status(err.status || 500)
+    res.status = err.status || 500
     res.send({
-        error: {
-            status: err.status || 500,
+        error:{
+            status: err.status || 500, // error object does not show status so we need to make that field
             message: err.message
         }
-    })
+    })  
 })
 
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, ()=>{
-    console.log(`Server running at port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`App running in PORT ${PORT}`)
 })
